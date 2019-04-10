4f358e7b29e8420da1ce8e6cd701044a0a6142ee
lzajac <lzajac@student.agh.edu.pl>
2019-04-10 22:06:06 +0200

    Create initial machine pool viewing component

---
+++ src/components/MachineDescription.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="program in description" :key="program.name">
+
+         <div id="program-tag">
+           <b-taglist attached>
+                        <b-tag type="is-dark">{{program.name}}</b-tag>
+                        <b-tag type="is-info">{{program.version}}</b-tag>
+            </b-taglist>
+         </div>
+
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    description: Object
+  },
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+#program-tag {
+  padding: 2px;
+}
+</style>
