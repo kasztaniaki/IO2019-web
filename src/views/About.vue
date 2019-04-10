5ea9d97186ccd8f0d397e18e0bd3521cd20c289b
lzajac <lzajac@student.agh.edu.pl>
2019-04-10 22:06:06 +0200

    Create initial machine pool viewing component

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,15 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="container">
+    <MachinePool/>
   </div>
 </template>
+
+<script>
+import MachinePool from '@/components/MachinePool.vue'
+export default {
+  name: 'About',
+  components: {
+    MachinePool
+  }
+}
+</script>
