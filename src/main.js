d56fa0a86c9233bd3408ed973b9aed7b53a9d786
js <js@js-pc>
2019-04-10 22:35:08 +0200

    Add fontawesome  to Vue app

--- src/main.js
+++ src/main.js
@@ -4,7 +4,12 @@ import 'buefy/dist/buefy.css'
 import App from './App.vue'
 import router from './router'
 
-Vue.use(Buefy)
+import '@fortawesome/fontawesome-free/css/all.css'
+import '@fortawesome/fontawesome-free/css/fontawesome.css'
+
+Vue.use(Buefy, {
+  'defaultIconPack': 'fas'
+})
 
 Vue.config.productionTip = false
 
