515a6891dcb9c59d872b2bd6ce3a69931be28900
js <jacek.starz@gmail.com>
2019-04-24 18:06:04 +0200

    Add global axios instance

--- src/main.js
+++ src/main.js
@@ -3,9 +3,12 @@ import Buefy from 'buefy'
 import 'buefy/dist/buefy.css'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
 
 import '@fortawesome/fontawesome-free/css/all.css'
 import '@fortawesome/fontawesome-free/css/fontawesome.css'
+
+Vue.prototype.$http = axios
 
 Vue.use(Buefy, {
   'defaultIconPack': 'fas'
@@ -15,5 +18,6 @@ Vue.config.productionTip = false
 
 new Vue({
   router,
+  axios,
   render: h => h(App)
 }).$mount('#app')
