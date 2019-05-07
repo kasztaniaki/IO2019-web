d8eb191822f92e4d125aed644bdf48177503988b
lzajac <lzajac@student.agh.edu.pl>
2019-05-07 16:40:21 +0200

    sorting done

--- src/components/MachinePool.vue
+++ src/components/MachinePool.vue
@@ -3,16 +3,16 @@
     <div class="level">
       <ImportButton v-on:import="loadMachinesData()" class="level-left"/>
     </div>
-    <b-table class="container" :data="data.machines" :columns="columnss">
+    <b-table class="container" :data="data.machines">
       <template slot-scope="props">
-        <b-table-column field="poolID" label="ID">{{props.row.PoolID}}</b-table-column>
-        <b-table-column field="displayName" label="Name">{{props.row.DisplayName}}</b-table-column>
-        <b-table-column
-          field="operatingSystem"
+        <b-table-column sortable field="PoolID" label="ID">{{props.row.PoolID}}</b-table-column>
+        <b-table-column sortable field="DisplayName" label="Name">{{props.row.DisplayName}}</b-table-column>
+        <b-table-column sortable
+          field="OperatingSystem"
           label="OS"
-        >{{props.row.OSName}} {{props.row.OSVersion}}</b-table-column>
-        <b-table-column field="maximumCount" label="Maximum Count">{{props.row.MaximumCount}}</b-table-column>
-        <b-table-column field="enabled" label="Enabled">
+        >{{props.row.OSName}}</b-table-column>
+        <b-table-column sortable field="MaximumCount" label="Maximum Count">{{props.row.MaximumCount}}</b-table-column>
+        <b-table-column field="Enabled" label="Enabled">
           <b-icon
             id="enabled-icon"
             v-if="props.row.Enabled"
@@ -22,7 +22,7 @@
           ></b-icon>
           <b-icon v-else id="disabled-icon" pack="fas" icon="times-circle" size="is-small"></b-icon>
         </b-table-column>
-        <b-table-column field="description" label="Description">
+        <b-table-column field="Description" label="Description">
           <MachineDescription :description="props.row.InstalledSoftware"/>
         </b-table-column>
       </template>
@@ -39,8 +39,6 @@ export default {
       this.$http
         .get('http://127.0.0.1:5000/pools')
         .then(response => {
-          console.log(response.data.pools)
-
           this.data.machines = response.data.pools
         })
         .catch(error => {
@@ -48,39 +46,11 @@ export default {
         })
     }
   },
-  watch: {
-    machines: function (oldValue, newValue) {}
-  },
   data () {
     return {
       data: {
         machines: []
-      },
-      columnss: [
-        {
-          field: 'poolID',
-          label: 'ID',
-          width: '40',
-          numeric: true
-        },
-        {
-          field: 'displayName',
-          label: 'Name'
-        },
-        {
-          field: 'maximumCount',
-          label: 'Maximum count'
-        },
-        {
-          field: 'enabled',
-          label: 'Enabled',
-          centered: true
-        },
-        {
-          field: 'description',
-          label: 'Description'
-        }
-      ]
+      }
     }
   },
   mounted () {
