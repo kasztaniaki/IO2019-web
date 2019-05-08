582812b41d6546f6d777a894def80756054aaa55
lzajac <lzajac@student.agh.edu.pl>
2019-05-08 19:21:53 +0200

    filtering done

--- src/components/MachinePool.vue
+++ src/components/MachinePool.vue
@@ -1,14 +1,13 @@
 <template>
   <div>
-    
     <div class="level">
       <ImportButton v-on:import="loadMachinesData()" class="level-left"/>
-      <b-input placeholder="filter"></b-input>
+      <b-input v-model=query placeholder="filter"></b-input>
     </div>
-    <b-table class="container" :data="data.machines">
+    <b-table class="container" :data=filter>
       <template slot-scope="props">
-        <b-table-column sortable field="PoolID" label="ID">{{props.row.PoolID}}</b-table-column>
-        <b-table-column sortable field="DisplayName" label="Name">{{props.row.DisplayName}}</b-table-column>
+        <b-table-column sortable field="PoolID" label="ID" >{{props.row.PoolID}}</b-table-column>
+        <b-table-column sortable field="DisplayName" label="Name" v-html="$options.filters.highlight(props.row.DisplayName, query)">{{props.row.DisplayName}}</b-table-column>
         <b-table-column sortable
           field="OperatingSystem"
           label="OS"
@@ -38,11 +37,12 @@ import ImportButton from '@/components/ImportButton.vue'
 export default {
   methods: {
     loadMachinesData () {
-      this.data.loading=true
+      this.data.loading = true
       this.$http
         .get('http://127.0.0.1:5000/pools')
         .then(response => {
           this.data.machines = response.data.pools
+          console.log(this.data.machines)
         })
         .catch(error => {
           console.log(error)
@@ -53,15 +53,43 @@ export default {
     return {
       data: {
         machines: []
-      }
+      },
+      query: ''
+
     }
   },
   mounted () {
     this.loadMachinesData()
   },
+  computed: {
+    filter: function () {
+      var re = RegExp(this.query, 'i')
+      var result = []
+      for (const pool of this.data.machines) {
+        for (const prop in pool) {
+          if (pool.hasOwnProperty(prop)) {
+            const element = pool[prop]
+            if (element.toString().match(re)) {
+              result.push(pool)
+              break
+            }
+          }
+        }
+      }
+      return result
+    }
+  },
   components: {
     MachineDescription,
     ImportButton
+  },
+  filters: {
+    highlight: function (value, query) {
+      var re = RegExp(query, 'i')
+      return value.toString().replace(re, function (matchedText, a, b) {
+        return ('<span class="highlight">' + matchedText + '</span>')
+      })
+    }
   }
 }
 </script>
@@ -72,5 +100,8 @@ export default {
 }
 #disabled-icon {
   color: red;
+}
+#highlight{
+  background-color: #FFFF00;
 }
 </style>
