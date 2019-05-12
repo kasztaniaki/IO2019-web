1eb567e1662ce19201c5229b9ff150880382358e
js <js@js-pc>
2019-05-12 20:00:39 +0200

    Move editing from inline to modal window

--- src/components/MachinePool.vue
+++ src/components/MachinePool.vue
@@ -1,33 +1,29 @@
 <template>
   <div>
     <div class="level">
-      <ImportButton v-on:import="loadMachinesData()" class="level-left"/>
+      <ImportButton v-on:import="loadMachinesData()" class="level-left" :disabled="isLoading"/>
+      <b-button v-if="editable" class="level-right" icon-left="plus" type="is-success" :disabled="isLoading" @click.native="editPool({})">New pool</b-button>
     </div>
-    <b-table class="container" :data="data.machines" :columns="columnss">
+    <b-table class="container" :data="data.machines" :columns="columns" :loading="isLoading">
       <template slot-scope="props">
         <b-table-column field="poolID" label="ID">
-          <b-input v-if="editable && selectedColumn === props.row.PoolID" v-model="props.row.PoolID"></b-input>
-          <template v-else>{{props.row.PoolID}}</template>
+          {{props.row.PoolID}}
         </b-table-column>
         <b-table-column field="displayName" label="Name">
-          <b-input v-if="editable && selectedColumn === props.row.PoolID" v-model="props.row.DisplayName"></b-input>
-          <template v-else>{{props.row.DisplayName}}</template>
+          {{props.row.DisplayName}}
         </b-table-column>
         <b-table-column field="operatingSystem" label="OS">
           <div class="columns is-vcentered">
             <div class="column">
-              <b-input v-if="editable && selectedColumn === props.row.PoolID" v-model="props.row.OSName"></b-input>
-              <template v-else>{{props.row.OSName}}</template>
+              {{props.row.OSName}}
             </div>
             <div class="column is-4">
-              <b-input v-if="editable && selectedColumn === props.row.PoolID" v-model="props.row.OSVersion"></b-input>
-              <template v-else>{{props.row.OSVersion}}</template>
+              {{props.row.OSVersion}}
             </div>
           </div>
         </b-table-column>
         <b-table-column field="maximumCount" label="Maximum Count">
-          <b-input v-if="editable && selectedColumn === props.row.PoolID" v-model="props.row.MaximumCount"></b-input>
-          <template v-else>{{props.row.MaximumCount}}</template>
+          {{props.row.MaximumCount}}
         </b-table-column>
         <b-table-column field="enabled" label="Enabled">
           <b-icon
@@ -43,16 +39,10 @@
           <MachineDescription :description="props.row.InstalledSoftware"/>
         </b-table-column>
         <b-table-column field="edit" :visible="editable">
-          <b-button v-if="selectedColumn !== props.row.PoolID" icon-left="edit" type="is-light" @click.native="editPool(props.row.PoolID)">
-            Edit
-          </b-button>
-          <b-button v-else icon-left="check" type="is-success" @click.native="savePool(props.row.PoolID)">
-            Save
-          </b-button>
+          <b-button icon-left="edit" type="is-light" @click.native="editPool(props.row)"></b-button>
         </b-table-column>
         <b-table-column field="remove" :visible="editable">
           <b-button icon-left="trash" type="is-danger" @click.native="removePool(props.row.PoolID)">
-            Delete
           </b-button>
         </b-table-column>
       </template>
@@ -63,22 +53,25 @@
 <script>
 import MachineDescription from '@/components/MachineDescription.vue'
 import ImportButton from '@/components/ImportButton.vue'
+import EditPoolForm from '@/components/EditPoolForm.vue'
+
 export default {
   methods: {
     loadMachinesData () {
+      this.isLoading = true
+
       this.$http
         .get('http://127.0.0.1:5000/pools')
         .then(response => {
           console.log(response.data.pools)
 
           this.data.machines = response.data.pools
+          this.isLoading = false
         })
         .catch(error => {
           console.log(error)
         })
     },
-    editPool (poolID) {
-      this.selectedColumn = poolID
     editPool (poolProps) {
       this.$modal.open({
         parent: this,
@@ -91,11 +84,10 @@ export default {
           }
         }
       })
+    },
+    removePool (poolID) {
       console.log('Remove pool ' + poolID)
     }
-  },
-  watch: {
-    machines: function (oldValue, newValue) {}
   },
   data () {
     return {
@@ -126,7 +118,8 @@ export default {
           field: 'description',
           label: 'Description'
         }
-      ]
+      ],
+      isLoading: false
     }
   },
   mounted () {
@@ -134,10 +127,14 @@ export default {
   },
   components: {
     MachineDescription,
-    ImportButton
+    ImportButton,
+    // eslint-disable-next-line
+    EditPoolForm
   },
   props: {
-    editable: Boolean
+    editable: {
+      type: Boolean
+    }
   }
 }
 </script>
