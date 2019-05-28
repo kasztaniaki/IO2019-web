8b7a1e9eb41c1bb099ac600815dfc8c23e0f534c
lzajac <lzajac@student.agh.edu.pl>
2019-05-28 14:45:37 +0200

    bugfix

--- src/components/MachinePool.vue
+++ src/components/MachinePool.vue
@@ -209,7 +209,7 @@ export default {
           position: 'is-bottom',
           type: 'is-success'
         })
-        loadMachinesData()
+        this.loadMachinesData()
       })
         .catch(error => {
           if (error) {
