1c1a7003cde2999590e2c7dc87c69b16b530e7e8
Jacek <jacek.starz@gmail.com>
2019-06-13 12:55:12 +0200

    Add confirm dialogs for issue actions

--- src/components/IssueList.vue
+++ src/components/IssueList.vue
@@ -9,7 +9,7 @@
               <span> Opened <b-tag rounded> {{ this.openedIssues.length }} </b-tag> </span>
             </template>
             <article v-for="issue in openedIssues" :key="issue.IssueID" class="message is-info">
-              <div class="message-header">
+              <div class="message-header" style="height: 50px">
                 <p>{{ issue.Subject }}</p>
                 <div class="buttons">
                   <b-button v-if="isAdmin"
@@ -17,13 +17,13 @@
                     size="is-small"
                     icon-left="check"
                     visi
-                    @click.native="resolveIssue(issue.IssueID)">
+                    @click.native="resolveIssueConfirm(issue.IssueID)">
                   </b-button>
                   <b-button
                     rounded
                     size="is-small"
                     icon-left="times"
-                    @click.native="rejectIssue(issue.IssueID)">
+                    @click.native="rejectIssueConfirm(issue.IssueID)">
                   </b-button>
                 </div>
               </div>
@@ -39,14 +39,14 @@
               <span>Resolved<b-tag rounded> {{ this.resolvedIssues.length }}</b-tag></span>
             </template>
             <article v-for="issue in resolvedIssues" :key="issue.IssueID" class="message is-success">
-              <div class="message-header">
+              <div class="message-header" style="height: 50px">
                 <p>{{ issue.Subject }}</p>
                 <div class="buttons">
                   <b-button v-if="isAdmin"
                     rounded
                     size="is-small"
                     icon-left="undo"
-                    @click.native="reopenIssue(issue.IssueID)">
+                    @click.native="reopenIssueConfirm(issue.IssueID)">
                   </b-button>
                 </div>
               </div>
@@ -62,14 +62,14 @@
               <span>Rejected<b-tag rounded> {{ this.rejectedIssues.length }}</b-tag></span>
             </template>
             <article v-for="issue in rejectedIssues" :key="issue.IssueID" class="message is-danger">
-              <div class="message-header">
+              <div class="message-header" style="height: 50px">
                 <p>{{ issue.Subject }}</p>
                 <div class="buttons">
                   <b-button v-if="isAdmin"
                     rounded
                     size="is-small"
                     icon-left="undo"
-                    @click.native="reopenIssue(issue.IssueID)">
+                    @click.native="reopenIssueConfirm(issue.IssueID)">
                   </b-button>
                 </div>
               </div>
@@ -102,6 +102,16 @@ export default {
         })
         .catch(error => this.handleError(error))
     },
+    resolveIssueConfirm (issueID) {
+      this.$dialog.confirm({
+        title: 'Resolving issue',
+        message: 'Are you sure you want to <b>resolve</b> this issue?',
+        confirmText: 'Resolve',
+        type: 'is-success',
+        hasIcon: true,
+        onConfirm: () => this.resolveIssue(issueID)
+      })
+    },
     resolveIssue (issueID) {
       resolveIssueReq(issueID)
         .then(response => {
@@ -114,6 +124,16 @@ export default {
         })
         .catch(error => this.handleError(error))
     },
+    rejectIssueConfirm (issueID) {
+      this.$dialog.confirm({
+        title: 'Rejecting issue',
+        message: 'Are you sure you want to <b>reject</b> this issue?',
+        confirmText: 'Reject',
+        type: 'is-danger',
+        hasIcon: true,
+        onConfirm: () => this.rejectIssue(issueID)
+      })
+    },
     rejectIssue (issueID) {
       rejectIssueReq(issueID)
         .then(response => {
@@ -125,6 +145,16 @@ export default {
           })
         })
         .catch(error => this.handleError(error))
+    },
+    reopenIssueConfirm (issueID) {
+      this.$dialog.confirm({
+        title: 'Reopening issue',
+        message: 'Are you sure you want to <b>reopen</b> this issue?',
+        confirmText: 'Reopen',
+        type: 'is-info',
+        hasIcon: true,
+        onConfirm: () => this.reopenIssue(issueID)
+      })
     },
     reopenIssue (issueID) {
       reopenIssueReq(issueID)
